f5c3f0a9c5241128302c21602bb6f95b6b3d50c7
Razeth <jamesadamhunt@outlook.com>
2013-02-26 14:06:06 +0000

    Version 0.2.1: Updated TestPlayer class to test with multiple values

--- MonopolyGame/src/TestPlayer.java
+++ MonopolyGame/src/TestPlayer.java
@@ -12,16 +12,32 @@ public class TestPlayer {
 		
 		returnedString+="Player Test Class \n\r";
 		returnedString+="setPiece() \n\r";
-		returnedString+="If working, should set Player's piece to 2 \n\r";
+		returnedString+="If working, should set Player's piece to 0, 1 and 2, using getPiece to return the values. \n\r";
+		PlayerTest.setPiece(0);
+		returnedString+=PlayerTest.getPiece()+"\n\r";
+		PlayerTest.setPiece(1);
+		returnedString+=PlayerTest.getPiece()+"\n\r";
 		PlayerTest.setPiece(2);
 		returnedString+=PlayerTest.getPiece()+"\n\r";
 		returnedString+="\n\raddMoney() \n\r";
-		returnedString+="If working, should add 500 making the Player's money 1000. \n\r";
+		returnedString+="If working, should add 250, 500, 750 and 1000 making the Player's money 4000. \n\r";
+		PlayerTest.addMoney(250);
+		returnedString+=PlayerTest.getMoney()+"\n\r";
 		PlayerTest.addMoney(500);
 		returnedString+=PlayerTest.getMoney()+"\n\r";
+		PlayerTest.addMoney(750);
+		returnedString+=PlayerTest.getMoney()+"\n\r";
+		PlayerTest.addMoney(1000);
+		returnedString+=PlayerTest.getMoney()+"\n\r";
 		returnedString+="\n\rsubMoney() \n\r";
-		returnedString+="If working, should subtract 500 making the Player's money 500. \n\r";
+		returnedString+="If working, should subtract 250, 500, 750 and 1000 making the Player's money 1500. \n\r";
+		PlayerTest.subMoney(250);
+		returnedString+=PlayerTest.getMoney()+"\n\r";
 		PlayerTest.subMoney(500);
+		returnedString+=PlayerTest.getMoney()+"\n\r";
+		PlayerTest.subMoney(750);
+		returnedString+=PlayerTest.getMoney()+"\n\r";
+		PlayerTest.subMoney(1000);
 		returnedString+=PlayerTest.getMoney()+"\n\r";
 		
 		returnedString+="\n\rgetName() \n\r";
